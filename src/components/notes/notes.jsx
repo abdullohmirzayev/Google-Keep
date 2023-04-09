00c1515017f247ab7f80ff9a774606f1ec37fd6f
Abdulloh <abdullohmirzayev7570@gmail.com>
2023-04-09 16:14:48 +0500

    added new component note

--- src/components/notes/notes.jsx
+++ src/components/notes/notes.jsx
@@ -1,20 +1,33 @@
+import { useContext } from 'react';
+
 import { styled } from '@mui/material/styles'
 import { Box } from '@mui/material'
 
 // copmonents
 import Form from './form';
+import Note from './note';
+
+import { DataContext } from '../../context/dataContext';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
 const Notes = () => {
+
+  const { notes } = useContext(DataContext);
+
   return (
     <>
-      <Box sx={{ flexGrow: 1, p: 3 }}>
-        <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <Box component='main' sx={{ p: 3, width: '100%' }}>
           <DrawerHeader />
           <Form />
+          {
+            notes.map(map => (
+              <Note />
+            ))
+          }
         </Box>
       </Box>
     </>
